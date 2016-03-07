972c68b31dd84eb0fceb07b3bdff51cf35a74b59
Ian Jenkins <ian.jenkins@boxuk.com>
2016-03-07 15:43:57 +0000

    WIP: Gaufrette adapter for flysystem

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flysystem Adapter for Gaufrette
+
+TODO.
---
+++ tests/GaufretteAdapterTest.php
@@ -0,0 +1,99 @@
+<?php
+
+class GaufretteAdapterTest extends PHPUnit_Framework_TestCase
+{
+    public function testWrite()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testWriteStream()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testUpdate()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testUpdateStream()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testRename()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testCopy()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testDelete()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testDeleteDir()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testCreateDir()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testSetVisibility()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testHas()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testRead()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testReadStream()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testListContents()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testGetMetadata()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testGetSize()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testGetMimetype()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testGetTimestamp()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    public function testGetVisiblity()
+    {
+        $this->markTestIncomplete('TODO');
+    }    
+}
